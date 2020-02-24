b794ba49688d3241e4cc85e46ac3e8801b35e150
BIT <BIT@DESKTOP-HU43P3A>
2020-02-24 17:17:49 +0900

    guestbook 완료

--- mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,7 +39,12 @@ public class GuestbookController {
 		model.addAttribute("no",no);
 		return "guestbook/delete";
 	}
-	
+
+/*	@RequestMapping(value="/delete/{no}",method=RequestMethod.GET)
+	public String delete(@ModelAttribute @PathVariable("no") Long no) {
+		return "guestbook/delete";
+	}
+*/	
 	@RequestMapping(value="/delete/{no}",method=RequestMethod.POST)
 	public String delete(
 			@PathVariable("no") Long no,
